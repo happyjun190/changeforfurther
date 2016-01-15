03576b7188eb993572ce8f7621e1a845dc2bbbf9
shenjun-company <shenjun@126.com>
2016-01-15 15:58:42 +0800

    for redis java

---
+++ src/main/java/com/mavenweb/service/redisexercise/TestRedisConnector.java
@@ -0,0 +1,22 @@
+package com.mavenweb.service.redisexercise;
+
+import redis.clients.jedis.Jedis;
+
+public class TestRedisConnector {
+	private static Jedis jedis;
+
+	public static void main(String args[]) {
+		jedis = new Jedis("localhost");
+		System.out.println("Connection to redis server successfully");
+		//查看服务器是否运行
+		System.out.println("Server is running: "+ jedis.ping());
+		
+		//redis set value
+		jedis.set("w3ckey", "Redis tutorial");
+		
+		
+		//redis get value
+		System.out.println("Stored string in redis:"+jedis.get("w3ckey"));
+		
+	}
+}
