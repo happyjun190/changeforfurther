ea882f34df6e39c35a72e79908988450f093d04e
shenjun-home <shenjun-home@126.com>
2016-05-10 23:47:23 +0800

    aop test

--- src/main/java/com/mavenweb/service/aoptest/impl/HelloWorldAspect.java
+++ src/main/java/com/mavenweb/service/aoptest/impl/HelloWorldAspect.java
@@ -1,23 +1,35 @@
 package com.mavenweb.service.aoptest.impl;
 
+import java.util.Map;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
 
 @Aspect
 @Component
 public class HelloWorldAspect {
 	
 	// 前置通知
-        //@Before("execution(public JsonResult com.mavenweb.service.caigou.impl.CaigouSolrService.getCaigouBySolr(Map<String, Object>")
-	public void beforeAdvice() {
-		System.out.println("===========before advice");
+        @Before("execution(* com.mavenweb.service.caigou.ICaigouSolrService.getCaigouBySolr(..))")
+	public void beforeAdvice(JoinPoint joinPoint) {
+            String methodName = joinPoint.getSignature().getName();
+            Object[] args = joinPoint.getArgs();
+            for(Object arg:args) {
+                System.err.println(arg.toString());
+            }
+            
+            System.out.println("start:the current method name was : " + methodName);
 	}
 
 	
 	// 后置最终通知
-	public void afterFinallyAdvice() {
-		System.out.println("===========after finally advice");
+        @AfterReturning("execution(* com.mavenweb.service.caigou.ICaigouSolrService.getCaigouBySolr(..))")
+	public void afterFinallyAdvice(JoinPoint joinPoint) {
+            String methodName = joinPoint.getSignature().getName();
+            System.out.println("end:the current method name was : " + methodName);
 	}
 	
 }
